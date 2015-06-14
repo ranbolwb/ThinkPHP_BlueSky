e1f203bc81d9f0fbefa21a517b4e7b61a0770e77
ranbolwb <lwbpeter@sina.com>
2015-06-14 11:00:12 +0800

    添加用户菜单显示

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -10,11 +10,71 @@ class IndexController extends Controller {
 
     public  function  main(){
         $user = session("user");
-        $this->assign('user',$user);
-        $this->display();
+        if($user == null){
+            $this->display("index");
+        }else{
+            $this->assign('user',$user);
+            $this->display();
+        }
     }
     public function menu(){
+        $user = session("user");
+        //是否超级管理员
+        $dao = M();
+        $sql = "select 1 from think_user_role a,think_role b where a.roleid=b.id and b.super=1 and a.userid=".$user["id"];
+        $list = $dao->query($sql);
+        if($list != null){
+            $sql = "select * from think_menu order by code";
+        }else {
+            $sql = "select c.* from think_user_role a,think_role_menu b,think_menu c where a.roleid=b.roleid and b.menuid=c.id and a.userid=" . $user["id"]." order by c.code";
+        }
+        $list = $dao->query($sql);
+        if($list != null){
+            $menu = array();
+            foreach ($list as $item) {
+                if(strlen($item["code"]) == 2){
+                    $menu[] = $item;
+                }
+            }
+            $this->assign('menu',$menu);
+            $this->assign('menuJson',$this->menuTree($list));
+        }
         $this->display();
+    }
+
+    private function menuTree($list){
+        $list = session("menu");
+        $menu = array();
+        foreach ($list as $item) {
+            if(strlen($item["code"]) == 2){
+                $d = array("id"=>$item["id"],"text"=>$item["title"],"children"=> $this->getMenuChild($item["code"],$list));
+                $menu[] = $d;
+            }
+        }
+        if(count($menu) > 0){
+            $j = json_encode($menu);
+            //exit($j);
+            return $j;
+        }else{
+            //exit("");
+            return "";
+        }
+    }
+
+    public function getMenuChild($code,$list){
+        $list = session("menu");
+        $menu = array();
+        foreach ($list as $item) {
+            if(strlen($item["code"]) == strlen($code) + 2 && substr($item["code"],0,strlen($code)) == $code) {
+                $d = array("id" => $item["id"], "text" => $item["title"],"attributes" => array("url" => $item["url"]), "children" => $this->getMenuChild($item["code"],$list));
+                $menu[] = $d;
+            }
+        }
+        if(count($menu) > 0){
+            return $menu;
+        }else{
+            return "";
+        }
     }
 
     public  function  changePassword(){
@@ -31,16 +91,24 @@ class IndexController extends Controller {
             session("user.password",$data["password"]);
             $result["state"] = 1;
             $result["msg"] = "修改密码成功！";
-            $this->ajaxReturn($result);
+            //$this->ajaxReturn($result);
+            echo(json_encode($result));
         }else{
             $result["state"] = 0;
             $result["msg"] = "原密码不正确！";
-            $this->ajaxReturn($result);
+            //$this->ajaxReturn($result);
+            echo(json_encode($result));
         }
     }
 
     public function loginOut(){
-        session("user",null);
+        //session("user",null);
+        $_SESSION = array();
+        if (isset($_COOKIE[session_name()])) {
+            setcookie(session_name(), '', time()-42000, '/');
+        }
+        // 最后彻底销毁session.
+        session_destroy();
         $this->display("index");
         //$this->redirect("index");
     }
@@ -60,10 +128,10 @@ class IndexController extends Controller {
         $list = $user->where($map)->bind($bind)->select();
         if(count($list) == 1){
             $currentUser = current($list);
-            $currentUser -> lasttime = date('Y-m-d H:i:s',time());
+            $currentUser["lasttime"] = date('Y-m-d H:i:s',time());
             session("user",$currentUser);
             //保存最后登录时间
-            $user->field('lasttime')->save($currentUser);
+            $user->field('lasttime,id')->save($currentUser);
             //保存登录日志
             $ul = M("user_login");
             $log["uid"] = $currentUser["id"];
@@ -73,11 +141,13 @@ class IndexController extends Controller {
 
             $data["state"] = 1;
             $data["url"] = U("Admin/Index/main");
-            $this->ajaxReturn($data);
+            //$this->ajaxReturn($data);
+            exit(json_encode($data));
         }else{
             $data["state"] = 0;
             $data["msg"] = "用户名或密码不正确！";
-            $this->ajaxReturn($data);
+            //$this->ajaxReturn($data);
+            exit(json_encode($data));
         }
     }
 
